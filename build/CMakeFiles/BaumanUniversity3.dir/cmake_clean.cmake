11f21d14e91aa7283b8a14ec3919d1803d3a426b
Klim <dert97975@gmail.com>
2024-05-25 09:42:39 +0300

    исправил мини приложение

---
+++ build/CMakeFiles/BaumanUniversity3.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "BaumanUniversity3.exe"
+  "BaumanUniversity3.exe.manifest"
+  "BaumanUniversity3.pdb"
+  "CMakeFiles/BaumanUniversity3.dir/src/main.cpp.obj"
+  "CMakeFiles/BaumanUniversity3.dir/src/main.cpp.obj.d"
+  "CMakeFiles/BaumanUniversity3.dir/src/main_window.cpp.obj"
+  "CMakeFiles/BaumanUniversity3.dir/src/main_window.cpp.obj.d"
+  "libBaumanUniversity3.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BaumanUniversity3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
